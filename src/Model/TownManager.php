00a0203cf3ad13fbdb5c2c3cf126221f2eee4c92
CeliaBoiss <boisseau.celia@gmail.com>
2020-10-31 23:00:11 +0100

    Remove some dead code in townManager

--- src/Model/TownManager.php
+++ src/Model/TownManager.php
@@ -26,48 +26,4 @@ class TownManager extends AbstractManager
     {
         parent::__construct(self::TABLE);
     }
-
-
-    /**
-     * @param array $item
-     * @return int
-     */
-    public function insert(array $item): int
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (`title`) VALUES (:title)");
-        $statement->bindValue('title', $item['title'], \PDO::PARAM_STR);
-
-        if ($statement->execute()) {
-            return (int)$this->pdo->lastInsertId();
-        }
-    }
-
-
-    /**
-     * @param int $id
-     */
-    /*public function delete(int $id): void
-    {
-        // prepared request
-        $statement = $this->pdo->prepare("DELETE FROM " . self::TABLE . " WHERE id=:id");
-        $statement->bindValue('id', $id, \PDO::PARAM_INT);
-        $statement->execute();
-    }*/
-
-
-    /**
-     * @param array $item
-     * @return bool
-     */
-    /*public function update(array $item):bool
-    {
-
-        // prepared request
-        $statement = $this->pdo->prepare("UPDATE " . self::TABLE . " SET `title` = :title WHERE id=:id");
-        $statement->bindValue('id', $item['id'], \PDO::PARAM_INT);
-        $statement->bindValue('title', $item['title'], \PDO::PARAM_STR);
-
-        return $statement->execute();
-    }*/
 }
