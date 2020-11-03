53b507dc1ee9f03ef89ac50b132dcf2b59e7a472
Kevin Roig <kevinroig37@gmail.com>
2020-11-03 19:58:37 +0100

    finally manage to have the informations we want in the overlay

--- src/Controller/RequestController.php
+++ src/Controller/RequestController.php
@@ -27,6 +27,7 @@ class RequestController extends AbstractController
     {
         $requestManager = new RequestManager();
         $requests = $requestManager->selectAllRequests();
+
         if ($requests === null) {
             echo 'Problème sur la base de données';
         }
--- src/Model/RequestManager.php
+++ src/Model/RequestManager.php
@@ -30,8 +30,16 @@ class RequestManager extends AbstractManager
     public function selectAllRequests(): ?array
     {
         try {
-             $statement = $this->pdo->query('SELECT * FROM ' . self::TABLE .
-            ' JOIN ' . UserManager::TABLE . ' ON user.id = fk_requester_id')->fetchAll();
+             $statement = $this->pdo->query('SELECT request.id AS requestId, user.firstname AS userFirstName, user.lastname AS userLastName,
+              request.title AS requestTitle, request.description AS requestDescription,
+              request.quantity AS requestQuantity, measurement.name AS measurementName, 
+              user.phone AS userPhone, user.email AS userEmail, address.street AS addressStreet, town.name AS townName,
+              town.postal_code AS townPostalCode, request.publication_date AS requestPublicationDate FROM ' . self::TABLE .
+            ' JOIN ' . UserManager::TABLE . ' ON user.id = fk_requester_id ' . 
+            ' JOIN ' . AddressManager::TABLE . ' ON address.id = fk_town_id ' .
+            ' JOIN ' . TownManager::TABLE . ' ON town.id = fk_address_id ' .
+            ' JOIN ' . MeasurementManager::TABLE . ' ON measurement.id = fk_measurement_id')->fetchAll();
+
         } catch (\PDOException $error) {
             return null;
         }
