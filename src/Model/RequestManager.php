feffb9f91739c6bce8f72f45bc91b675ac636fb7
CeliaBoiss <boisseau.celia@gmail.com>
2020-11-10 10:21:31 +0100

    Modif of RequestManager : selectAllRequests now display the results in order of date, the newer on first

--- src/Model/RequestManager.php
+++ src/Model/RequestManager.php
@@ -38,25 +38,26 @@ class RequestManager extends AbstractManager
     public function selectAllRequests(): ?array
     {
         try {
-             $results = $this->pdo->query('SELECT request.id AS requestId, user.firstname AS userFirstName, 
-              user.lastname AS userLastName,
-              request.title AS requestTitle, request.description AS requestDescription,
-              request.quantity AS requestQuantity, measurement.name AS measurementName, 
-              user.phone AS userPhone, user.email AS userEmail, address.street AS addressStreet, 
-              town.name AS townName, 
-              town.postal_code AS townPostalCode, request.publication_date AS requestPublicationDate 
-              FROM ' . self::TABLE .
+            $results = $this->pdo->query('SELECT request.id AS requestId, user.firstname AS userFirstName, 
+            user.lastname AS userLastName,
+            request.title AS requestTitle, request.description AS requestDescription,
+            request.quantity AS requestQuantity, measurement.name AS measurementName, 
+            user.phone AS userPhone, user.email AS userEmail, address.street AS addressStreet, 
+            town.name AS townName, 
+            town.postal_code AS townPostalCode, request.publication_date AS requestPublicationDate 
+            FROM ' . self::TABLE .
             ' LEFT JOIN ' . UserManager::TABLE . ' ON user.id = fk_requester_id ' .
             ' LEFT JOIN ' . AddressManager::TABLE . ' ON address.id = fk_address_id ' .
             ' LEFT JOIN ' . TownManager::TABLE . ' ON town.id = fk_town_id ' .
-            ' LEFT JOIN ' . MeasurementManager::TABLE . ' ON measurement.id = fk_measurement_id')->fetchAll();
+            ' LEFT JOIN ' . MeasurementManager::TABLE . ' ON measurement.id = fk_measurement_id
+            ORDER BY requestPublicationDate DESC')->fetchAll();
         } catch (\PDOException $error) {
             return null;
         }
         return $results;
     }
 
-    public function insert(Request $request): ?bool
+    public function insert(Request $request): bool
     {
         $statement = $this->pdo->prepare("INSERT INTO " . self::TABLE . " (title,quantity,
         description,publication_date,fk_requester_id,fk_measurement_id) VALUES (:title,:quantity,
